a33cf837baad65db5a80e59c40a2cded0570c9b5
BENOITMEZAGUER <benoit.mezaguer@gmail.com>
2024-05-28 15:00:07 +0200

    quete 4 ok

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -10,14 +10,20 @@ router.get("/", sayWelcome);
 // Import And Use Routers Here
 /* ************************************************************************* */
 
-const itemsRouter = require("./items/router");
-
-router.use("/items", itemsRouter);
+/* Add these *************************************************************** */
+/* ************************************************************************* */
 
 const programsRouter = require("./programs/router");
 
 router.use("/programs", programsRouter);
 
+const itemsRouter = require("./items/router");
+
+router.use("/items", itemsRouter);
+
+const categoriesRouter = require("./categories/router");
+
+router.use("/categories", categoriesRouter);
 /* ************************************************************************* */
 
 module.exports = router;
